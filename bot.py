a01d06b01d8db97222a7a7f05c802388884141e0
Eyal Golan <eyalgolan96@gmail.com>
2021-10-23 19:47:43 +0300

    remove telegram token before making the repo public

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = '993360636:AAFiqvCi9bquQoOUKZTPhXf9NT7Ei3kaqko'
+TOKEN = '<telegram token>'
 
 emoji_money = "\U0001F911"
 emoji_sad = "\U0001F628"
